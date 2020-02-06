4d6976203cdfd79422e2452a1a027e5a06eca6a3
Flo <retorianer@gmail.com>
2020-02-06 18:55:00 +0100

    first commit

---
+++ src/main/java/com/favorite/places/favplaces/jwt/JwtInMemoryUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.favorite.places.favplaces.jwt;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@CrossOrigin(origins="http://localhost:3000")
+public class JwtInMemoryUserDetailsService implements UserDetailsService {
+
+  static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
+
+  static {
+    inMemoryUserList.add(new JwtUserDetails(1L, "kalydo",
+        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
+        .filter(user -> user.getUsername().equals(username)).findFirst();
+
+    if (!findFirst.isPresent()) {
+      throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+    }
+
+    return findFirst.get();
+  }
+
+}
+
+
